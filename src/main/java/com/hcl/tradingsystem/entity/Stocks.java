9cbaf9ad5684d076b67b718c12a58131ea467b63
user1 <user1@LP-5GXW8R1.HCLT.CORP.HCL.IN>
2019-05-16 13:43:46 +0530

    initial commit

---
+++ src/main/java/com/hcl/tradingsystem/entity/Stocks.java
@@ -0,0 +1,49 @@
+package com.hcl.tradingsystem.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="stocks")
+public class Stocks implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long StockId;	
+	private String ComapnyName;
+	private Double Price;
+	private Integer StockQty;
+	public Long getStockId() {
+		return StockId;
+	}
+	public void setStockId(Long stockId) {
+		StockId = stockId;
+	}
+	public String getComapnyName() {
+		return ComapnyName;
+	}
+	public void setComapnyName(String comapnyName) {
+		ComapnyName = comapnyName;
+	}
+	public Double getPrice() {
+		return Price;
+	}
+	public void setPrice(Double price) {
+		Price = price;
+	}
+	public Integer getStockQty() {
+		return StockQty;
+	}
+	public void setStockQty(Integer stockQty) {
+		StockQty = stockQty;
+	}	
+}
---
+++ src/main/java/com/hcl/tradingsystem/entity/User.java
@@ -0,0 +1,36 @@
+package com.hcl.tradingsystem.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="stocks")
+public class User implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;	
+	private String userName;
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+}
